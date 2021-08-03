cc6f52fe6cfef553c81197844bc1b5d659a50b2b
MichelledeGroot <michelle@rdegroot.net>
2021-08-03 10:12:00 +0200

    update barplot

--- R/stacked_bar.R
+++ R/stacked_bar.R
@@ -7,38 +7,44 @@
 #' @param csv_file Path to the input folder
 #' @import ggplot2
 #' @import RColorBrewer
+#' @import reshape2
 #' @export
 
-compare_runs <- function(csv_folder, taxon_level="species", size_col="Total_reads"){
+compare_runs <- function(csv_folder, taxon_level="species", size_col="Total_reads", type="value"){
+    # create a dataset
+    filenames <- list.files(csv_folder, pattern="*.csv", full.names=TRUE)
 
-  # create a dataset
-  filenames <- list.files(csv_folder, pattern="*.csv", full.names=TRUE)
+    for (file in filenames){
+      run = unlist(strsplit(basename(file), ".csv"))[1]
 
-  for (file in filenames){
-    run = unlist(strsplit(basename(file), ".csv"))[1]
+      # if the merged dataset doesn't exist, create it
+      if (!exists("dataset")){
+        dataset <- read.csv(file, header=TRUE)
+        dataset['run']=run
+      }
 
-    # if the merged dataset doesn't exist, create it
-    if (!exists("dataset")){
-      dataset <- read.csv(file, header=TRUE)
-      dataset['run']=run
+      # if the merged dataset does exist, append to it
+      if (exists("dataset")){
+        temp_dataset <-read.csv(file, header=TRUE)
+        temp_dataset['run']=run
+        dataset<-rbind(dataset, temp_dataset)
+        rm(temp_dataset)
+      }
     }
 
-    # if the merged dataset does exist, append to it
-    if (exists("dataset")){
-      temp_dataset <-read.csv(file, header=TRUE)
-      temp_dataset['run']=run
-      dataset<-rbind(dataset, temp_dataset)
-      rm(temp_dataset)
-    }
-  }
+    reshaped <- dcast(dataset, run ~ eval(as.name(taxon_level)), value.var=size_col,fun.aggregate = sum)
+    nb <- length(unique(dataset[,taxon_level]))
+    mycolors <- colorRampPalette(brewer.pal(12, "Paired"))(nb)
 
-  # Stacked + percent
-  nb <- length(unique(dataset[,taxon_level]))
-  mycolors <- colorRampPalette(brewer.pal(12, "Paired"))(nb)
-  ggplot(dataset, aes(fill=eval(as.name(taxon_level)), y=eval(as.name(size_col)), x=run)) +
-    geom_bar(position="fill", stat="identity") +
-    scale_fill_manual(values = mycolors) +
-    theme(axis.text.x = element_text(angle = 90)) +
-    scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
-    labs(fill=taxon_level, y=size_col)
+    reshape2::melt(reshaped, id.vars='run') %>%
+      group_by(run) %>%
+      mutate(pct = value / sum(value), pct = as.numeric(scales::percent(pct, accuracy = 0.01,suffix = ""))) %>%
+      plot_ly(x = ~run, y = ~eval(as.name(type)), type = 'bar',
+              text = ~paste(variable,
+                            "<br> Percentage: ",pct, "%",
+                            "<br> Total: ", value),
+              name = ~variable, textposition = "none", hoverinfo = 'text',
+              color = ~variable, colors = mycolors) %>%
+      layout(yaxis = list(title = type), barmode = 'stack',
+             xaxis = list(tickangle = 45))
 }
